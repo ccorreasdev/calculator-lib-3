5a32684d621851d601a8d1d5e14ee1bd993b8cc4
ccorreas <correasberenguer@gmail.com>
2026-02-12 08:54:52 +0100

    feat: Añadir la función de prohibición de números negativos #4

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,8 @@
 public class Calculator {
     public int add(int a, int b) {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException();
+        }
         return a + b;
     }
 }
