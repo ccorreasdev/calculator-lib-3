c2a7501f9a87b37617d523333d5fcc3b46b75904
ccorreas <correasberenguer@gmail.com>
2026-02-12 08:55:47 +0100

    feat: Añadido el mensaje de números aleatorios no permitidos #4

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,7 @@
 public class Calculator {
     public int add(int a, int b) {
         if (a < 0 || b < 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Negative numbers not allowed");
         }
         return a + b;
     }
